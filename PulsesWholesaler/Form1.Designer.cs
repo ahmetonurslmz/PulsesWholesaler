0d74291e1b39cbc6b38460311d97121df93f0a0d
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-05-15 17:40:33 +0300

    completes front of customer information groupbox.

--- PulsesWholesaler/Form1.Designer.cs
+++ PulsesWholesaler/Form1.Designer.cs
@@ -31,10 +31,29 @@
             this.groupBoxCustomerInformation = new System.Windows.Forms.GroupBox();
             this.groupBoxStockStatus = new System.Windows.Forms.GroupBox();
             this.groupBoxSalesDetails = new System.Windows.Forms.GroupBox();
+            this.lblCustomerTcId = new System.Windows.Forms.Label();
+            this.txtCustomerTcId = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblCustomerFullName = new System.Windows.Forms.Label();
+            this.lblPulsesType = new System.Windows.Forms.Label();
+            this.txtPulsesQuantity = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboPulsesType = new System.Windows.Forms.ComboBox();
+            this.btnAddSales = new System.Windows.Forms.Button();
+            this.groupBoxCustomerInformation.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBoxCustomerInformation
             // 
+            this.groupBoxCustomerInformation.Controls.Add(this.btnAddSales);
+            this.groupBoxCustomerInformation.Controls.Add(this.comboPulsesType);
+            this.groupBoxCustomerInformation.Controls.Add(this.txtPulsesQuantity);
+            this.groupBoxCustomerInformation.Controls.Add(this.label4);
+            this.groupBoxCustomerInformation.Controls.Add(this.lblPulsesType);
+            this.groupBoxCustomerInformation.Controls.Add(this.textBox1);
+            this.groupBoxCustomerInformation.Controls.Add(this.lblCustomerFullName);
+            this.groupBoxCustomerInformation.Controls.Add(this.txtCustomerTcId);
+            this.groupBoxCustomerInformation.Controls.Add(this.lblCustomerTcId);
             this.groupBoxCustomerInformation.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBoxCustomerInformation.Location = new System.Drawing.Point(12, 12);
             this.groupBoxCustomerInformation.Name = "groupBoxCustomerInformation";
@@ -63,6 +82,80 @@
             this.groupBoxSalesDetails.TabStop = false;
             this.groupBoxSalesDetails.Text = "Sales Details";
             // 
+            // lblCustomerTcId
+            // 
+            this.lblCustomerTcId.AutoSize = true;
+            this.lblCustomerTcId.Location = new System.Drawing.Point(35, 46);
+            this.lblCustomerTcId.Name = "lblCustomerTcId";
+            this.lblCustomerTcId.Size = new System.Drawing.Size(128, 18);
+            this.lblCustomerTcId.TabIndex = 0;
+            this.lblCustomerTcId.Text = "Customer T.C. ID:";
+            // 
+            // txtCustomerTcId
+            // 
+            this.txtCustomerTcId.Location = new System.Drawing.Point(169, 43);
+            this.txtCustomerTcId.Name = "txtCustomerTcId";
+            this.txtCustomerTcId.Size = new System.Drawing.Size(119, 24);
+            this.txtCustomerTcId.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 76);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 24);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblCustomerFullName
+            // 
+            this.lblCustomerFullName.AutoSize = true;
+            this.lblCustomerFullName.Location = new System.Drawing.Point(14, 76);
+            this.lblCustomerFullName.Name = "lblCustomerFullName";
+            this.lblCustomerFullName.Size = new System.Drawing.Size(149, 18);
+            this.lblCustomerFullName.TabIndex = 2;
+            this.lblCustomerFullName.Text = "Customer Full Name:";
+            // 
+            // lblPulsesType
+            // 
+            this.lblPulsesType.AutoSize = true;
+            this.lblPulsesType.Location = new System.Drawing.Point(70, 112);
+            this.lblPulsesType.Name = "lblPulsesType";
+            this.lblPulsesType.Size = new System.Drawing.Size(93, 18);
+            this.lblPulsesType.TabIndex = 4;
+            this.lblPulsesType.Text = "Pulses Type:";
+            // 
+            // txtPulsesQuantity
+            // 
+            this.txtPulsesQuantity.Location = new System.Drawing.Point(169, 139);
+            this.txtPulsesQuantity.Name = "txtPulsesQuantity";
+            this.txtPulsesQuantity.Size = new System.Drawing.Size(119, 24);
+            this.txtPulsesQuantity.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Pulses Quantity:";
+            // 
+            // comboPulsesType
+            // 
+            this.comboPulsesType.FormattingEnabled = true;
+            this.comboPulsesType.Location = new System.Drawing.Point(169, 107);
+            this.comboPulsesType.Name = "comboPulsesType";
+            this.comboPulsesType.Size = new System.Drawing.Size(119, 26);
+            this.comboPulsesType.TabIndex = 8;
+            // 
+            // btnAddSales
+            // 
+            this.btnAddSales.Location = new System.Drawing.Point(294, 137);
+            this.btnAddSales.Name = "btnAddSales";
+            this.btnAddSales.Size = new System.Drawing.Size(84, 29);
+            this.btnAddSales.TabIndex = 9;
+            this.btnAddSales.Text = "Add Sales";
+            this.btnAddSales.UseVisualStyleBackColor = true;
+            // 
             // PulsesWholesaler
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -73,6 +166,8 @@
             this.Controls.Add(this.groupBoxCustomerInformation);
             this.Name = "PulsesWholesaler";
             this.Text = "Pulses Wholesaler";
+            this.groupBoxCustomerInformation.ResumeLayout(false);
+            this.groupBoxCustomerInformation.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +177,15 @@
         private System.Windows.Forms.GroupBox groupBoxCustomerInformation;
         private System.Windows.Forms.GroupBox groupBoxStockStatus;
         private System.Windows.Forms.GroupBox groupBoxSalesDetails;
+        private System.Windows.Forms.Button btnAddSales;
+        private System.Windows.Forms.ComboBox comboPulsesType;
+        private System.Windows.Forms.TextBox txtPulsesQuantity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblPulsesType;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCustomerFullName;
+        private System.Windows.Forms.TextBox txtCustomerTcId;
+        private System.Windows.Forms.Label lblCustomerTcId;
     }
 }
 
