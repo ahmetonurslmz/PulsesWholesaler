0f9a184db40a526669885a869412b86befd5bae2
Ahmet Onur Solmaz <ahmetonurslmz@gmail.com>
2020-05-17 15:14:00 +0300

    adds pulse stock relation to procedure that fetch sales

--- migration/procedures/dbo.Procedure.fetchSales.sql
+++ migration/procedures/dbo.Procedure.fetchSales.sql
@@ -1,4 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[PfetchSales]
 AS
-	SELECT * FROM [dbo].[Sales]
+	SELECT customer_id, customer_tc_id, customer_full_name, pulse_name, pulse_quantity
+	FROM [dbo].[Sales]
+	INNER JOIN [dbo].[PulseStocks]
+	ON [dbo].[Sales].pulse_type_id = [dbo].[PulseStocks].pulse_stock_id
 RETURN 0
